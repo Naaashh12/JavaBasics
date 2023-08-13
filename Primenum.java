2de204f3e0d8a79323877c970098d268102a536f
Naaashh12 <nashratjahan12@gmail.com>
2023-08-13 12:07:59 +0530

    added

--- Primenum.java
+++ Primenum.java
@@ -2,22 +2,28 @@ import java.util.*;
 
 public class Primenum {
     public static void main(String[] args) {
-        System.out.println("Enter range of numbers ");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    
         System.out.println("Enter a number to check ");
+        Scanner sc= new Scanner(System.in);
         int num = sc.nextInt();
-        int flag=0;
-        for(int i=2; i<n;i++) {
-            if(num%i!=0&& ) {
-                flag=1;
-            }
+        Boolean flag=true;
+        if(num ==2)
+        {
+            System.out.println("The number is prime");
         }
-        if(flag==1) {
-            System.out.println("The number is prime"+num);
+        else {
+        for(int i=2; i<num-1;i++) {
+            if(num%i== 0 ) {
+                flag=false;
+            }
+    
+        }
+        if(flag==true) {
+            System.out.println("The number is prime");
         }
         else {
             System.out.println("The number is not prime");
         }
     }
+}
 }
