88f1e3051405db79e2b7a8ce4e42d70c55b44cc3
Naaashh12 <nashratjahan12@gmail.com>
2023-08-17 09:38:06 +0530

    Arrays added

--- Stock.java
+++ Stock.java
@@ -1,9 +1,25 @@
 public class Stock {
-    public static void predictstock() {
+    public static void predictstock(int sp[]) {
+        
+        int bp=Integer.MAX_VALUE;
+        int profit=Integer.MIN_VALUE;
+        int maxprofit=0;
+        for(int i=0;i<sp.length;i++){
+        if(sp[i]>bp) {
+            profit=sp[i]-bp;
+            if(profit>maxprofit)
+            maxprofit=profit;
+        }
+        else
+            bp=sp[i];
 
+    }
+    System.out.print("Maxprofit is: " + maxprofit);
     }
 
     public static void main(String[] args) {
         
+        int[] sp={7,1,5,3,6,4};
+        predictstock(sp);
     }
 }
